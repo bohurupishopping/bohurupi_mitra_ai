31b8f55560468d1b64bd8d6c6acb5086bd7b7d8b
Bohurupi Shopping <official.bohurupi@gmail.com>
2024-11-13 12:44:11 +0530

    Removed demosrc from version control

--- demosrc/components/ui/use-toast.ts
+++
@@ -1,3 +0,0 @@
-import { useToast, toast } from "@/components/use-toast";
-
-export { useToast, toast };
--- demosrc/types/image.ts
+++
@@ -1,9 +0,0 @@
-export type ImageSize = "1024x1024" | "256x256" | "512x512" | "1792x1024" | "1024x1792";
-
-export interface ImageGenerationParams {
-  model: string;
-  prompt: string;
-  negative_prompt?: string;
-  n: number;
-  size: ImageSize;
-} 
